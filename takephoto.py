e67bd64c1aa066b7437972ea98655f909c79d95c
studentAutomations <student.automations@gmail.com>
2024-11-22 20:03:38 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -43,11 +43,11 @@ try:
     page_to_scrape.find_element(By.LINK_TEXT, "Baze").click()
     time.sleep(5)
 
-    page_to_scrape.find_element(By.CLASS_NAME, "instancename").click()
+    page_to_scrape.find_element(By.LINK_TEXT, "News forum").click()
     time.sleep(5)
 
 
-        responseT = page_to_scrape.find_element(By.ID, "region-main")
+    responseT = page_to_scrape.find_element(By.CLASS_NAME, "region-main-content")
 
     # Get the size of the element
     height = responseT.size['height']
