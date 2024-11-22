c2a555ff2b42ae774181421619b23e8e51dc7a75
studentAutomations <student.automations@gmail.com>
2024-11-22 15:20:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,9 +46,6 @@ try:
     page_to_scrape.find_element(By.CLASS_NAME, "instancename").click()
     time.sleep(5)
 
-    page_to_scrape.find_element(By.LINK_TEXT, "News forum").click()
-    time.sleep(5)
-
 
     responseT = page_to_scrape.find_element(By.ID, "region-main")
 
