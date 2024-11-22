e78d2df04c6f51d5c4fbbe3174891b7f7ea4bacb
studentAutomations <student.automations@gmail.com>
2024-11-22 22:47:41 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -43,8 +43,7 @@ try:
     page_to_scrape.find_element(By.LINK_TEXT, "Baze").click()
     time.sleep(5)
 
-    link_element = page_to_scrape.find_element(By.XPATH, "//a[@https://cs.elfak.ni.ac.rs/nastava/mod/forum/view.php?id=104']")
-    link_element.click()
+    page_to_scrape.find_element(By.CLASS_NAME, "instancename").click()
     time.sleep(5)
 
 
