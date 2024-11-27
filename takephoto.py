20cdd74f54afbcd31bfeca9c5f9fb25d08c58e82
studentAutomations <student.automations@gmail.com>
2024-11-27 18:26:38 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -65,7 +65,7 @@ try:
     page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
 
     
-    responseT.screenshot('cs-baze-nova-obavestenja.png')
+    responseT.screenshot('https://github.com/studentAutomations/cs-baze/blob/56a5a7e9c6bd1cfc790f9b9985ba4879a0c723fd/cs-baze-nova-obavestenja.png')
 
 finally:
     
