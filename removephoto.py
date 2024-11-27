566f3de01bb92967513aa2157aea90408f3a806a
studentAutomations <student.automations@gmail.com>
2024-11-27 18:26:16 +0100

    Update removephoto.py

--- removephoto.py
+++ removephoto.py
@@ -1,7 +1,7 @@
 import os
 
 
-photo_path = 'cs-baze-nova-obavestenja.png'  
+photo_path = 'https://github.com/studentAutomations/cs-baze/blob/56a5a7e9c6bd1cfc790f9b9985ba4879a0c723fd/cs-baze-nova-obavestenja.png'  
 
 
 if os.path.exists(photo_path):
