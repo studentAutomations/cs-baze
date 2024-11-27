d835d6c88ed898bec87e1ae31a148ab9ec946be2
studentAutomations <student.automations@gmail.com>
2024-11-27 18:33:56 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -10,7 +10,7 @@ for url in WEBHOOK_URL:
 
     hook.send('**@everyone Nova obavestenja na CS-u!**')
     
-    image2_path = 'cs-baze-nova-obavestenja.png'  
+    image2_path = 'https://github.com/studentAutomations/cs-baze/blob/56a5a7e9c6bd1cfc790f9b9985ba4879a0c723fd/cs-baze-nova-obavestenja.png'  
     
     hook.send(file=File(image2_path, name='cs-baze-nova-obavestenja.png'))
 
