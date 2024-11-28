803559974eb6ab6bf875a2a048e0cf4a5c518878
studentAutomations <student.automations@gmail.com>
2024-11-28 01:33:44 +0100

    Delete removephoto1.py

--- removephoto1.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-photo_path = 'https://github.com/studentAutomations/cs-baze/blob/main/cs-baze1-nova-obavestenja.png'  
-
-
-if os.path.exists(photo_path):
-    os.remove(photo_path)  
