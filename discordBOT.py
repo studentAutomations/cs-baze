44cab7d6af2637415c538215be695661807e4fb0
studentAutomations <student.automations@gmail.com>
2024-12-10 10:08:39 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -8,7 +8,7 @@ for url in WEBHOOK_URL:
     hook = Webhook(url)
 
     embed = Embed(
-        description="**[CS link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/view.php?id=104)**",
+        description="**[CS link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=9&fromyear=2024&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
         color=0x3498DB
     )
 
