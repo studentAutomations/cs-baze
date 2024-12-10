876817585a7c6c6d8fba5dd8e0507cbd62d9f988
studentAutomations <student.automations@gmail.com>
2024-12-10 11:21:27 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -42,7 +42,7 @@ try:
 
     height = responseT.size['height']
     width = responseT.size['width']     
-    desired_width = max(width, 1200)  
+    desired_width = min(width, 1200)  
     desired_height = min(height, 1000)
     page_to_scrape.set_window_size(desired_width, desired_height)     
     page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)  
